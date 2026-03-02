7d5ecb75d7ef593553754a5dbd17fab7cf89db69
ahassa09 <ahassa09@uoguelph.ca>
2026-03-02 18:23:58 -0500

    help page skeleton

--- src/templates/static/js/routes.js
+++ src/templates/static/js/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import Help from './components/Help';
 
 export default (
   <HashRouter>
     <div>
       <Route exact path='/' component={Home} />
       <Route path='/about' component={About} />
+      <Route path='/help' component={Help} />
     </div>
   </HashRouter>
 );
