293e52bfa26c62b37bc26d378e47c6f662338586
ahassa09 <ahassa09@uoguelph.ca>
2026-03-03 17:28:43 -0500

    Help and About drafts

--- src/templates/static/js/routes.js
+++ src/templates/static/js/routes.js
@@ -4,12 +4,14 @@ import Home from './components/Home';
 import About from './components/About';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import Help from './components/Help';
 
 export default (
   <HashRouter>
     <div>
       <Route exact path='/' component={Home} />
       <Route path='/about' component={About} />
+      <Route path='/help' component={Help} />
       <Route path='/login' component={Login} />
       <Route path='/signup' component={SignUp} />
     </div>
